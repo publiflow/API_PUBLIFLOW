616dbad8d02d829a6db6a9cede062d16b6ba375d
the-kris-code <cris456mendes@gmail.com>
2025-10-05 00:40:26 -0300

    v2 testes unitarios

---
+++ tests/controllers/postagemController.test.ts
@@ -0,0 +1,150 @@
+import request from "supertest";
+import app from "../../src/index";
+import {prisma} from "../../src/database/prisma";
+import path from 'path';
+import fs from 'fs';
+
+let authorId: number;
+
+beforeEach(async () => {
+  await prisma.pF_postagem.deleteMany({});
+  await prisma.pF_usuario.deleteMany({});
+  await prisma.pF_papelUsuario.deleteMany({});
+
+  await prisma.pF_papelUsuario.create({
+    data: { id: 1, papelUsuario: 'Aluno' },
+  });
+
+  const author = await prisma.pF_usuario.create({
+    data: {
+      nomeCompleto: 'Cristhian Mendes',
+      telefone: '11933049341',
+      email: 'cris456mendes@gmail.com',
+      papelUsuarioID: 1,
+      senha: '123',
+    },
+  });
+  authorId = author.id; 
+});
+
+describe("Testando rota /api/posts", () => {
+  it("Criação de post", async () => {
+
+    const imagePath = path.join(__dirname, '..', 'assets', 'test-image.png');
+
+    if (!fs.existsSync(imagePath)) {
+      if (!fs.existsSync(path.dirname(imagePath))) {
+        fs.mkdirSync(path.dirname(imagePath), { recursive: true });
+      }
+      fs.writeFileSync(imagePath, 'fake image data');
+    }
+
+    const response = await request(app)
+      .post('/api/posts')
+      .field('titulo', 'Post criativo')
+      .field('descricao', 'Esta é a descrição do meu primeiro post.')
+      .field('visibilidade', 'true')
+      .field('autorID', authorId.toString())
+      .attach('imagem', imagePath);
+
+      expect(response.status).toBe(201);
+      expect(response.body.autorID).toEqual(authorId);
+      expect(response.body.titulo).toEqual("Post criativo");
+  });
+
+  it("Lista todos os posts", async () => {
+    const response = await request(app).get("api/posts");
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+   it("Busca post por id", async () => {
+    const postData = await prisma.pF_postagem.create({
+      data: {
+        titulo: 'teste',
+        descricao: 'teste',
+        visibilidade: true,
+        caminhoImagem: 'fake-path.jpg',
+        autorID: authorId,
+      },
+    });
+
+    const response = await request(app).get(`api/posts/${postData.id}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.id).toEqual(postData.id);
+  });
+
+   it("Atualizar post", async () => {
+    const postData = await prisma.pF_postagem.create({
+      data: {
+        titulo: 'teste',
+        descricao: 'teste',
+        visibilidade: true,
+        caminhoImagem: 'fake-path.jpg',
+        autorID: authorId,
+      },
+    });
+    
+    const updatedPostData = {
+      descricao: "descrição da postagem atualizada",
+    }
+
+    const response = await request(app).put(`api/posts/${postData.id}`).send(updatedPostData);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.descricao).toBe(updatedPostData.descricao);
+    });
+
+    it("Deletar post", async () => {
+    const postData = await prisma.pF_postagem.create({
+      data: {
+        titulo: 'teste',
+        descricao: 'teste',
+        visibilidade: true,
+        caminhoImagem: 'fake-path.jpg',
+        autorID: authorId,
+      },
+    });
+
+    const response = await request(app).delete(`api/posts/${postData.id}`);
+
+    expect(response.statusCode).toBe(204);
+    });
+
+    it("Listar feed com postagens visiveis", async () => {
+    const postData = await prisma.pF_postagem.create({
+      data: {
+        titulo: 'teste',
+        descricao: 'teste',
+        visibilidade: true,
+        caminhoImagem: 'fake-path.jpg',
+        autorID: authorId,
+      },
+    });
+    
+    const response = await request(app).get(`api/posts/feed`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0].id).toEqual(postData.id);
+    expect(Array.isArray(response.body)).toBe(true);
+    });
+ 
+    it("Busca post por titulo ou descrição", async () => {
+    const postData = await prisma.pF_postagem.create({
+      data: {
+        titulo: 'titulo criativo',
+        descricao: 'teste',
+        visibilidade: true,
+        caminhoImagem: 'fake-path.jpg',
+        autorID: authorId,
+      },
+    });
+    
+    const response = await request(app).get(`api/posts/search`).query("titulo criativo");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0].id).toEqual(postData.id);
+    });
+});
--- tests/controllers/userController.test.ts
+++ tests/controllers/userController.test.ts
@@ -1,85 +1,113 @@
 import request from "supertest";
-import app from "../../src/index"  
+import app from "../../src/index";
+import {prisma} from "../../src/database/prisma";
+
+beforeEach(async () => {
+  await prisma.pF_usuario.deleteMany({});
+});
 
 describe("Testando rota /api/users", () => {
   it("Criação de usuário", async () => {
     const userData = {
-    id: 1,
-    nome: 'cristhian',
-    sobrenome: 'mendes',
-    telefone: '11933049341',
-    email: 'cris456mendes@gmail.com',
-    papelUsuarioID: 1,
-    senha: '123',
-    dataCadastro: new Date(2025, 8, 28)
+      nome: 'cristhian',
+      sobrenome: 'mendes',
+      telefone: '11933049341',
+      email: 'cris456mendes@gmail.com',
+      papelUsuarioID: 1,
+      senha: '123',
     }
     const response = await request(app).post("/api/users").send(userData);
 
-    expect(response.body).toEqual(expect.objectContaining(userData));
+    const expectedResponse = {
+      ...userData,
+      nomeCompleto: `${userData.nome} ${userData.sobrenome}`,
+      id: expect.any(Number),
+      dataCadastro: expect.any(String),
+    };
+    //delete expectedResponse.nome;
+    //delete expectedResponse.sobrenome;
+    //delete expectedResponse.senha; // Senha não deve ser retornada
+
+    expect(response.body).toEqual(expect.objectContaining(expectedResponse));
     expect(response.status).toBe(201);
   });
 
   it("Lista usuário por id", async () => {
     const userData = {
-    id: 1,
-    nome: 'cristhian',
-    sobrenome: 'mendes',
-    telefone: '11933049341',
-    email: 'cris456mendes@gmail.com',
-    papelUsuarioID: 1,
-    senha: '123',
-    dataCadastro: new Date(2025, 8, 28)
+      nome: 'cristhian',
+      sobrenome: 'mendes',
+      telefone: '11933049341',
+      email: 'cris456mendes@gmail.com',
+      papelUsuarioID: 1,
+      senha: '123', 
     }
     const createUserResponse = await request(app).post("/api/users").send(userData);
     const userId = createUserResponse.body.id;
     const getUserResponse = await request(app).get(`/api/users/${userId}`);
     
+    const expectedResponse = {
+      ...userData,
+      nomeCompleto: `${userData.nome} ${userData.sobrenome}`,
+      id: userId,
+      dataCadastro: expect.any(String),
+    };
+    //delete expectedResponse.nome;
+    //delete expectedResponse.sobrenome;
+    //delete expectedResponse.senha; // Senha não deve ser retornada
+
     expect(getUserResponse.statusCode).toBe(200);
-    expect(getUserResponse.body).toEqual(expect.objectContaining(userData));
+    expect(getUserResponse.body).toEqual(expect.objectContaining(expectedResponse));
   });
 
   it("Lista todos os usuários", async () => {
-    const userData = {
-    id: 1,
-    nome: 'cristhian',
-    sobrenome: 'mendes',
-    telefone: '11933049341',
-    email: 'cris456mendes@gmail.com',
-    papelUsuarioID: 1,
-    senha: '123',
-    dataCadastro: new Date(2025, 8, 28)
-    }
-    const createUserResponse = await request(app).post("/api/users").send(userData);
     const getUserResponse = await request(app).get(`/api/users`);
     
     expect(getUserResponse.statusCode).toBe(200);
+    expect(Array.isArray(getUserResponse.body)).toBe(true);
   });
 
    it("Atualiza usuário", async () => {
     const userData = {
-    id: 1,
-    nome: 'cristhian',
-    sobrenome: 'mendes',
-    telefone: '11933049341',
-    email: 'cris456mendes@gmail.com',
-    papelUsuarioID: 1,
-    senha: '123',
-    dataCadastro: new Date(2025, 8, 28)
+      nome: 'cristhian',
+      sobrenome: 'mendes',
+      telefone: '11933049341',
+      email: 'cris456mendes@gmail.com',
+      papelUsuarioID: 1,
+      senha: '123',
+    }
+    const createUserResponse = await request(app).post("/api/users").send(userData);
+    const userId = createUserResponse.body.id;
+
+    const updatedUserData = {
+      email: 'email_novo@gmail.com',
+    }
+    const updateUserResponse = await request(app).put(`/api/users/${userId}`).send(updatedUserData);
+
+    expect(updateUserResponse.statusCode).toBe(200);
+    expect(updateUserResponse.body.email).toBe(updatedUserData.email);
+    
+  });
+
+  it("Deletar usuário", async () => {
+    const userData = {
+      nome: 'cristhian',
+      sobrenome: 'mendes',
+      telefone: '11933049341',
+      email: 'cris456mendes@gmail.com',
+      papelUsuarioID: 1,
+      senha: '123',
     }
     const createUserResponse = await request(app).post("/api/users").send(userData);
 
-    const updatedUserData = {
-    id: 1,
-    nome: 'João',
-    sobrenome: 'Gomes',
-    telefone: '11942567718',
-    email: 'joaozinho@gmail.com.br',
-    papelUsuarioID: 1,
-    senha: '123456',
-    dataCadastro: new Date(2025, 8, 28)
-    }
-    const getUserResponse = await request(app).get(`/api/users`);
+    const getUserResponse = await request(app).delete(`/api/users/${createUserResponse.body.id}`);
     
-    expect(getUserResponse.statusCode).toBe(200);
+    expect(getUserResponse.statusCode).toBe(204);
+  });
+
+    it("Rota root", async () => {
+    
+    const rootRoute = await request(app).get("/");
+    
+    expect(rootRoute.body).toEqual("API Publiflow - Bem-vindo!");
   });
 });
